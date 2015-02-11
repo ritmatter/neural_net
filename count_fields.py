c551fd4786ae4786320dc19c82bddebc6a83614d
ritmatter <mritter123@gmail.com>
2015-02-10 21:53:09 -0500

    Count fields and add more fields

---
+++ count_fields.py
@@ -0,0 +1,189 @@
+# Third-party libraries
+from pymongo import MongoClient
+import numpy as np
+
+def load_data():
+
+    # Connect to mongo
+    client = MongoClient('localhost', 27017)
+    db = client.yelp_data
+    businesses = db.businesses
+
+    # Get the example restaurant
+    print 'Take-out: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Take-out": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Take-out'));
+    print ''
+    print 'Good For.dessert: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.dessert": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.dessert'));
+    print ''
+
+    print 'Good For.latenight: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.latenight": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.latenight'));
+    print ''
+
+    print 'Good For.lunch: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.lunch": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.lunch'));
+    print ''
+
+    print 'Good For.dinner: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.dinner": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.dinner'));
+    print ''
+
+    print 'Good For.breakfast: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.breakfast": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.breakfast'));
+    print ''
+
+    print 'Good For.brunch: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For.brunch": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For.brunch'));
+    print ''
+
+    print 'Caters: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Caters": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Caters'));
+    print ''
+
+    print 'Noise Level: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Noise Level": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Noise Level'));
+    print ''
+
+    print 'Ambience.romantic: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.romantic'));
+    print ''
+
+    print 'Ambience.intimate: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.intimate": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.intimate'));
+    print ''
+
+    print 'Ambience.touristy: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.touristy": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.touristy'));
+    print ''
+
+    print 'Ambience.hipster: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.hipster": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.hipster'));
+    print ''
+
+    print 'Ambience.divey: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.divey": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.divey'));
+    print ''
+
+    print 'Ambience.classy: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.classy": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.classy'));
+    print ''
+
+    print 'Ambience.trendy: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.trendy": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.trendy'));
+    print ''
+
+    print 'Ambience.upscale: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.upscale": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.upscale'));
+    print ''
+
+    print 'Ambience.casual: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Ambience.casual": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Ambience.casual'));
+    print ''
+
+    print 'Parking.garage: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Parking.garage": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Parking.garage'));
+    print ''
+
+    print 'Parking.street: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Parking.street": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Parking.street'));
+    print ''
+
+    print 'Parking.validated: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Parking.validated": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Parking.validated'));
+    print ''
+
+    print 'Parking.lot: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Parking.lot": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Parking.lot'));
+    print ''
+
+    print 'Parking.valet: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Parking.valet": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Parking.valet'));
+    print ''
+
+    print 'Has TV: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Has TV": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Has TV'));
+    print ''
+
+    print 'Outdoor Seating: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Outdoor Seating": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Outdoor Seating'));
+    print ''
+
+    print 'Attire: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Attire": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Attire'));
+    print ''
+
+    print 'Alcohol: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Alcohol": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Alcohol'));
+    print ''
+
+    print 'Waiter Service: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Waiter Service": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Waiter Service'));
+    print ''
+
+    print 'Accepts Credit Cards: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Accepts Credit Cards": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Accepts Credit Cards'));
+    print ''
+
+    print 'Good for Kids: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good for Kids": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good for Kids'));
+    print ''
+
+    print 'Takes Reservations: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Takes Reservations": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Takes Reservations'));
+    print ''
+
+    print 'Delivery: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Delivery": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Delivery'));
+    print ''
+
+    print 'Good For Groups: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Good For Groups": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Good For Groups'));
+    print ''
+
+    print 'Price Range: ' + str(businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Price Range": { "$exists": True }} ]}).count());
+    print 'Distinct values: ' + str(businesses.distinct('attributes.Price Range'));
+    print ''
+
+
+    print 'Combined: ' + str(businesses.find({ "$and": [
+      {"categories": "Restaurants"},
+      { "attributes.Take-out": { "$exists": True }},
+      { "attributes.Good For.dessert": { "$exists": True }},
+      { "attributes.Good For.lunch": { "$exists": True }},
+      { "attributes.Good For.latenight": { "$exists": True }},
+      { "attributes.Good For.dinner": { "$exists": True }},
+      { "attributes.Good For.breakfast": { "$exists": True }},
+      { "attributes.Good For.brunch": { "$exists": True }},
+      { "attributes.Noise Level": { "$exists": True }},
+
+      { "attributes.Ambience.romantic": { "$exists": True }},
+      { "attributes.Ambience.intimate": { "$exists": True }},
+      { "attributes.Ambience.touristy": { "$exists": True }},
+      { "attributes.Ambience.hipster": { "$exists": True }},
+      { "attributes.Ambience.divey": { "$exists": True }},
+      { "attributes.Ambience.classy": { "$exists": True }},
+      { "attributes.Ambience.trendy": { "$exists": True }},
+      { "attributes.Ambience.upscale": { "$exists": True }},
+      { "attributes.Ambience.casual": { "$exists": True }},
+
+      { "attributes.Parking.garage": { "$exists": True }},
+      { "attributes.Parking.street": { "$exists": True }},
+      { "attributes.Parking.lot": { "$exists": True }},
+      { "attributes.Parking.validated": { "$exists": True }},
+      { "attributes.Parking.valet": { "$exists": True }},
+
+      { "attributes.Has TV": { "$exists": True }},
+      { "attributes.Outdoor Seating": { "$exists": True }},
+      { "attributes.Attire": { "$exists": True }},
+      { "attributes.Takes Reservations": { "$exists": True }},
+      { "attributes.Delivery": { "$exists": True }},
+      { "attributes.Alcohol": { "$exists": True }},
+      { "attributes.Waiter Service": { "$exists": True }},
+      { "attributes.Accepts Credit Cards": { "$exists": True }},
+      { "attributes.Good for Kids": { "$exists": True }},
+      { "attributes.Good For Groups": { "$exists": True }},
+      { "attributes.Price Range": { "$exists": True }},
+    ] }).count());
+
+load_data();
--- yelp_loader.py
+++ yelp_loader.py
@@ -21,7 +21,42 @@ def load_data():
     # Query for all of the restaurants that contain relevant fields
     #restaurants = businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Take-out": { "$exists": "True" }}, { "attributes.Caters": { "$exists": "True" }}, { "attributes.Takes Reservations": { "$exists": "True" }}, { "attributes.Delivery": { "$exists": "True" }}, { "attributes.Has TV": { "$exists": "True" }}, { "attributes.Good For Groups": { "$exists": "True" }}, { "attributes.Outdoor Seating": { "$exists": "True" }}, { "attributes.Good For.dinner": { "$exists": "True" }} ] })
 
-    restaurants = businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Take-out": { "$exists": True }}, { "attributes.Caters": { "$exists": True }}, { "attributes.Takes Reservations": { "$exists": True }}, { "attributes.Delivery": { "$exists": True }}, { "attributes.Has TV": { "$exists": True }}, { "attributes.Good For Groups": { "$exists": True }}, { "attributes.Outdoor Seating": { "$exists": True }}, { "attributes.Good For.dinner": { "$exists": True }}, { "attributes.Price Range": { "$exists": True }}, { "attributes.Alcohol": { "$exists": True }} ] })
+    restaurants = businesses.find({ "$and": [
+      {"categories": "Restaurants"},
+      { "attributes.Take-out": { "$exists": True }},
+      { "attributes.Good For.dessert": { "$exists": True }},
+      { "attributes.Good For.lunch": { "$exists": True }},
+      { "attributes.Good For.latenight": { "$exists": True }},
+      { "attributes.Good For.dinner": { "$exists": True }},
+      { "attributes.Good For.breakfast": { "$exists": True }},
+      { "attributes.Good For.brunch": { "$exists": True }},
+      { "attributes.Noise Level": { "$exists": True }},
+      { "attributes.Ambience.romantic": { "$exists": True }},
+      { "attributes.Ambience.intimate": { "$exists": True }},
+      { "attributes.Ambience.touristy": { "$exists": True }},
+      { "attributes.Ambience.hipster": { "$exists": True }},
+      { "attributes.Ambience.divey": { "$exists": True }},
+      { "attributes.Ambience.classy": { "$exists": True }},
+      { "attributes.Ambience.trendy": { "$exists": True }},
+      { "attributes.Ambience.upscale": { "$exists": True }},
+      { "attributes.Ambience.casual": { "$exists": True }},
+      { "attributes.Parking.garage": { "$exists": True }},
+      { "attributes.Parking.street": { "$exists": True }},
+      { "attributes.Parking.lot": { "$exists": True }},
+      { "attributes.Parking.validated": { "$exists": True }},
+      { "attributes.Parking.valet": { "$exists": True }},
+      { "attributes.Has TV": { "$exists": True }},
+      { "attributes.Takes Reservations": { "$exists": True }},
+      { "attributes.Delivery": { "$exists": True }},
+      { "attributes.Outdoor Seating": { "$exists": True }},
+      { "attributes.Attire": { "$exists": True }},
+      { "attributes.Alcohol": { "$exists": True }},
+      { "attributes.Waiter Service": { "$exists": True }},
+      { "attributes.Accepts Credit Cards": { "$exists": True }},
+      { "attributes.Good for Kids": { "$exists": True }},
+      { "attributes.Good For Groups": { "$exists": True }},
+      { "attributes.Price Range": { "$exists": True }},
+    ] });
 
     training_data = []
     test_data = []
@@ -30,24 +65,84 @@ def load_data():
 
         # Get various attributes out of the data results
         take_out = 1 if restaurant["attributes"]["Take-out"] else 0
-        caters = 1 if restaurant["attributes"]["Caters"] else 0
+        good_for_dessert = 1 if restaurant["attributes"]["Good For"]["dessert"] else 0
+        good_for_lunch = 1 if restaurant["attributes"]["Good For"]["lunch"] else 0
+        good_for_latenight = 1 if restaurant["attributes"]["Good For"]["latenight"] else 0
+        good_for_dinner = 1 if restaurant["attributes"]["Good For"]["dinner"] else 0
+        good_for_breakfast = 1 if restaurant["attributes"]["Good For"]["breakfast"] else 0
+        good_for_brunch = 1 if restaurant["attributes"]["Good For"]["brunch"] else 0
+        noise_level = get_noise_level(restaurant["attributes"]["Noise Level"])
+
+        ambience_romantic = 1 if restaurant["attributes"]["Ambience"]["romantic"] else 0
+        ambience_intimate = 1 if restaurant["attributes"]["Ambience"]["intimate"] else 0
+        ambience_touristy = 1 if restaurant["attributes"]["Ambience"]["touristy"] else 0
+        ambience_hipster = 1 if restaurant["attributes"]["Ambience"]["hipster"] else 0
+        ambience_divey = 1 if restaurant["attributes"]["Ambience"]["divey"] else 0
+        ambience_classy = 1 if restaurant["attributes"]["Ambience"]["classy"] else 0
+        ambience_trendy = 1 if restaurant["attributes"]["Ambience"]["trendy"] else 0
+        ambience_upscale = 1 if restaurant["attributes"]["Ambience"]["upscale"] else 0
+        ambience_casual = 1 if restaurant["attributes"]["Ambience"]["casual"] else 0
+
+        parking_garage = 1 if restaurant["attributes"]["Parking"]["garage"] else 0
+        parking_street = 1 if restaurant["attributes"]["Parking"]["street"] else 0
+        parking_lot = 1 if restaurant["attributes"]["Parking"]["lot"] else 0
+        parking_validated = 1 if restaurant["attributes"]["Parking"]["validated"] else 0
+        parking_valet = 1 if restaurant["attributes"]["Parking"]["valet"] else 0
+
         takes_reservations = 1 if restaurant["attributes"]["Takes Reservations"] else 0
         delivery = 1 if restaurant["attributes"]["Delivery"] else 0
         has_TV = 1 if restaurant["attributes"]["Has TV"] else 0
-        good_for_groups = 1 if restaurant["attributes"]["Good For Groups"] else 0
         outdoor_seating = 1 if restaurant["attributes"]["Outdoor Seating"] else 0
-        good_for_dinner = 1 if restaurant["attributes"]["Good For"]["dinner"] else 0
+        attire = get_attire(restaurant["attributes"]["Attire"])
+        alcohol = get_alcohol(restaurant["attributes"]["Alcohol"])
+        waiter_service = 1 if restaurant["attributes"]["Waiter Service"] else 0
+        accepts_credit_cards = 1 if restaurant["attributes"]["Accepts Credit Cards"] else 0
+        good_for_kids = 1 if restaurant["attributes"]["Good for Kids"] else 0
+        good_for_groups = 1 if restaurant["attributes"]["Good For Groups"] else 0
 
-        (cheap, moderate, pricey, expensive) = get_price_range(restaurant["attributes"]["Price Range"])
-        (no_alcohol, full_bar, beer_and_wine) = get_alcohol(restaurant["attributes"]["Alcohol"])
+        price_range = restaurant["attributes"]["Price Range"]
 
         # Get the stars and number of reviews for the restaurant
         stars = restaurant["stars"]
         review_count = restaurant["review_count"]
 
-        attributes = np.array([[take_out, caters, takes_reservations, delivery, has_TV, good_for_groups,
-          outdoor_seating, good_for_dinner, cheap, moderate, pricey, expensive, no_alcohol, full_bar, beer_and_wine]])
+        attributes = np.array([[
+          take_out,
+          good_for_dessert,
+          good_for_lunch,
+          good_for_latenight,
+          good_for_dinner,
+          good_for_breakfast,
+          good_for_brunch,
+          noise_level,
+          ambience_romantic,
+          ambience_intimate,
+          ambience_touristy,
+          ambience_hipster,
+          ambience_divey,
+          ambience_classy,
+          ambience_trendy,
+          ambience_upscale,
+          ambience_casual,
+          parking_garage,
+          parking_street,
+          parking_lot,
+          parking_validated,
+          parking_valet,
+          takes_reservations,
+          delivery,
+          has_TV,
+          outdoor_seating,
+          attire,
+          alcohol,
+          waiter_service,
+          accepts_credit_cards,
+          good_for_kids,
+          good_for_groups,
+          price_range,
+        ]])
         attributes = np.transpose(attributes)
+
         data_entry = (attributes, restaurant_score(stars, review_count))
 
         # Use the first 6800 results for taining data and the rest for test data
@@ -62,25 +157,34 @@ def load_data():
 
     return (training_data, test_data)
 
+# Returns noise level rating (average, quiet, loud, very_loud)
+def get_noise_level(noise_level):
+    if noise_level == "average":
+      return 0
+    elif noise_level == "quiet":
+      return 1
+    elif noise_level == "loud":
+      return 2
+    elif noise_level == "very_loud":
+      return 3
+
 # Returns alcohol rating (none, full_bar, beer_and_wine)
 def get_alcohol(alcohol):
     if alcohol == "none":
-      return (1, 0, 0)
+      return 0
     elif alcohol == "full_bar":
-      return (0, 1, 0)
+      return 1
     elif alcohol == "beer_and_wine":
-      return (0, 0, 1)
+      return 2
 
-# Returns attributes for price range (cheap, moderate, pricey, expensive)
-def get_price_range(price_range):
-    if price_range == 1:
-      return (1, 0, 0, 0)
-    elif price_range == 2:
-      return (0, 1, 0, 0)
-    elif price_range == 3:
-      return (0, 0, 1, 0)
-    elif price_range == 4:
-      return (0, 0, 0, 1)
+# Returns attire rating (casual, dressy, formal)
+def get_attire(attire):
+    if attire == "casual":
+      return 0
+    elif attire == "dressy":
+      return 1
+    elif attire == "formal":
+      return 2
 
 # Returns the label for a restaurant based on stars and review count
 def restaurant_score(stars, review_count):

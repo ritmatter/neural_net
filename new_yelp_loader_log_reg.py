b9f3950ebc4bb8b618a00d0d117ea3ccc637b74e
jf3ng <jason21feng@gmail.com>
2015-03-03 20:57:58 -0500

    logistic regresion for dummy new yelp

--- new_yelp_loader_log_reg.py
+++ new_yelp_loader_log_reg.py
@@ -1,11 +1,15 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 """
 mnist_loader
 ~~~~~~~~~~~~
 A library to load the MNIST image data.  For details of the data
 structures that are returned, see the doc strings for ``load_data``
-and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
+and ``load_data_wrapper``.    In practice, ``load_data_wrapper`` is the
 function usually called by our neural network code.
 """
+
+# Standard libraries
+import random
 
 # Third-party libraries
 from pymongo import MongoClient
@@ -13,13 +17,11 @@ import numpy as np
 
 def load_data():
 
+    TEST_SIZE = 2000
     # Connect to mongo
     client = MongoClient('localhost', 27017)
-    db = client.yelp_data
+    db = client.new_yelp_data
     businesses = db.businesses
-
-    # Query for all of the restaurants that contain relevant fields
-    #restaurants = businesses.find({ "$and": [ {"categories": "Restaurants"}, { "attributes.Take-out": { "$exists": "True" }}, { "attributes.Caters": { "$exists": "True" }}, { "attributes.Takes Reservations": { "$exists": "True" }}, { "attributes.Delivery": { "$exists": "True" }}, { "attributes.Has TV": { "$exists": "True" }}, { "attributes.Good For Groups": { "$exists": "True" }}, { "attributes.Outdoor Seating": { "$exists": "True" }}, { "attributes.Good For.dinner": { "$exists": "True" }} ] })
 
     restaurants = businesses.find({ "$and": [
       {"categories": "Restaurants"},
@@ -30,37 +32,30 @@ def load_data():
       { "attributes.Good For.dinner": { "$exists": True }},
       { "attributes.Good For.breakfast": { "$exists": True }},
       { "attributes.Good For.brunch": { "$exists": True }},
-      { "attributes.Noise Level": { "$exists": True }},
-      { "attributes.Ambience.romantic": { "$exists": True }},
-      { "attributes.Ambience.intimate": { "$exists": True }},
-      { "attributes.Ambience.touristy": { "$exists": True }},
-      { "attributes.Ambience.hipster": { "$exists": True }},
-      { "attributes.Ambience.divey": { "$exists": True }},
-      { "attributes.Ambience.classy": { "$exists": True }},
-      { "attributes.Ambience.trendy": { "$exists": True }},
-      { "attributes.Ambience.upscale": { "$exists": True }},
-      { "attributes.Ambience.casual": { "$exists": True }},
       { "attributes.Parking.garage": { "$exists": True }},
       { "attributes.Parking.street": { "$exists": True }},
       { "attributes.Parking.lot": { "$exists": True }},
       { "attributes.Parking.validated": { "$exists": True }},
       { "attributes.Parking.valet": { "$exists": True }},
-      { "attributes.Has TV": { "$exists": True }},
-      { "attributes.Takes Reservations": { "$exists": True }},
-      { "attributes.Delivery": { "$exists": True }},
       { "attributes.Outdoor Seating": { "$exists": True }},
       { "attributes.Attire": { "$exists": True }},
-      { "attributes.Alcohol": { "$exists": True }},
-      { "attributes.Waiter Service": { "$exists": True }},
       { "attributes.Accepts Credit Cards": { "$exists": True }},
       { "attributes.Good for Kids": { "$exists": True }},
       { "attributes.Good For Groups": { "$exists": True }},
       { "attributes.Price Range": { "$exists": True }},
     ] });
 
+    # DUMMY VARIABLES:
+    # Ambience.*
+    # Noise Level
+    # Has TV
+    # Delivery
+    # Alcohol
+    # Waiter Service
+
+    data_matrix = []
     training_data = []
     test_data = []
-    labels = []
     i = 0
     for restaurant in restaurants:
 
@@ -74,15 +69,16 @@ def load_data():
         good_for_brunch = 1 if restaurant["attributes"]["Good For"]["brunch"] else 0
         noise_level = get_noise_level(restaurant["attributes"]["Noise Level"])
 
-        ambience_romantic = 1 if restaurant["attributes"]["Ambience"]["romantic"] else 0
-        ambience_intimate = 1 if restaurant["attributes"]["Ambience"]["intimate"] else 0
-        ambience_touristy = 1 if restaurant["attributes"]["Ambience"]["touristy"] else 0
-        ambience_hipster = 1 if restaurant["attributes"]["Ambience"]["hipster"] else 0
-        ambience_divey = 1 if restaurant["attributes"]["Ambience"]["divey"] else 0
-        ambience_classy = 1 if restaurant["attributes"]["Ambience"]["classy"] else 0
-        ambience_trendy = 1 if restaurant["attributes"]["Ambience"]["trendy"] else 0
-        ambience_upscale = 1 if restaurant["attributes"]["Ambience"]["upscale"] else 0
-        ambience_casual = 1 if restaurant["attributes"]["Ambience"]["casual"] else 0
+        (ambience_romatic,
+         ambience_intimate,
+         ambience_touristy,
+         ambience_hipter,
+         ambience_divey,
+         ambience_classy,
+         ambience_trendy,
+         ambience_upscale,
+         ambience_casual,
+        ) = getAmbienceFields(restaurants["attributes"]["Ambience"])
 
         parking_garage = 1 if restaurant["attributes"]["Parking"]["garage"] else 0
         parking_street = 1 if restaurant["attributes"]["Parking"]["street"] else 0
@@ -107,7 +103,7 @@ def load_data():
         stars = restaurant["stars"]
         review_count = restaurant["review_count"]
 
-        attributes = np.array([
+        attributes = np.array([[
           take_out,
           good_for_dessert,
           good_for_lunch,
@@ -141,23 +137,44 @@ def load_data():
           good_for_kids,
           good_for_groups,
           price_range,
-        ])
+        ]])
         attributes = np.transpose(attributes)
 
-        # data_entry = (attributes, restaurant_score(stars, review_count))
+        data_entry = (attributes, restaurant_score(stars, review_count))
 
-        # Use the first 6800 results for taining data and the rest for test data
-        if i < 6800:
-            training_data.append(attributes)
-            labels.append(restaurant_score(stars, review_count))
-        else:
-            test_data.append(attributes)
+        data_matrix.append(data_entry)
         i += 1
 
-    test_data = np.array(test_data)
-    training_data = np.array(training_data)
+    random.shuffle(data_matrix)
+    test_data = data_matrix[0:TEST_SIZE]
+    training_data = data_matrix[TEST_SIZE + 1: len(data_matrix)]
 
-    return (training_data, test_data, training_labels, testing_labels)
+    print("Class 1 count is:")
+    print(class1)
+    print("Class 0 count is:")
+    print(class0)
+    return (training_data, test_data)
+
+# Returns ambience fields
+# These may be 0.5 if they do not exist for the given restaurant
+def get_ambience_fields(ambience):
+    if not ambience:
+        return (0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
+
+    print("See what happens when the field is not present")
+    print(ambience["romantic"])
+    print(not not ambience["romantic"])
+    print("")
+
+    ambience_romantic = 1 if restaurant["attributes"]["Ambience"]["romantic"] else 0
+    ambience_intimate = 1 if restaurant["attributes"]["Ambience"]["intimate"] else 0
+    ambience_touristy = 1 if restaurant["attributes"]["Ambience"]["touristy"] else 0
+    ambience_hipster = 1 if restaurant["attributes"]["Ambience"]["hipster"] else 0
+    ambience_divey = 1 if restaurant["attributes"]["Ambience"]["divey"] else 0
+    ambience_classy = 1 if restaurant["attributes"]["Ambience"]["classy"] else 0
+    ambience_trendy = 1 if restaurant["attributes"]["Ambience"]["trendy"] else 0
+    ambience_upscale = 1 if restaurant["attributes"]["Ambience"]["upscale"] else 0
+    ambience_casual = 1 if restaurant["attributes"]["Ambience"]["casual"] else 0
 
 # Returns noise level rating (average, quiet, loud, very_loud)
 def get_noise_level(noise_level):
@@ -190,9 +207,17 @@ def get_attire(attire):
 
 # Returns the label for a restaurant based on stars and review count
 def restaurant_score(stars, review_count):
-    score = 0.7 * stars + 0.3 * review_count
-    if score > 18.5:
-        val = 1
+    global class1
+    global class0
+    val = np.zeros((2, 1))
+    if stars >= 3.5 and review_count > 37:
+        class1 += 1
+        val[1] = 1
     else:
-        val = 0;
+        class0 += 1
+        val[0] = 1
     return val
+
+class1 = 0
+class0 = 0
+

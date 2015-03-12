9609be23f359d33ef7a036c71430fb438509a5ab
Sudikoff Lab iMac <imac@dhcp-210-38.lab.cs.dartmouth.edu>
2015-03-12 08:23:27 -0400

    commiting new holdouts

--- new_test.py
+++ new_test.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-import loader_dummy_middle
+import loader_dummy_average
 import network
 
-(training_data, test_data) = loader_dummy_middle.load_data()
+(training_data, test_data) = loader_dummy_average.load_data()
 net = network.Network([68, 25, 25, 2])
 net.SGD(training_data, 20, 10, 0.5, test_data)
